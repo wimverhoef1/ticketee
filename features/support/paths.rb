dee1b356d15b9d802f1b736eaae440c422c0e8d4
Wim Verhoef <wimverhoef1@gmail.com>
2012-04-14 20:00:28 +0200

    Add validation to ensure names are specified when creating projects

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -12,7 +12,7 @@ module NavigationHelpers
                 '/'
 
             when /the project page for "([^\"]*)"/
-                project_path(project($1))
+                project_path(Project.find_by_name!($1))
             when /the "([^\"]*)" ticket in the "([^\"]*)" project/
                 project_ticket_path(project($2), Ticket.find_by_title($1))
 
